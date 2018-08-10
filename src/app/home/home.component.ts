ebfe6fb8de66b72c9090a2c8ecc43ef4ee51238b
Omer Farooq <omer.farooq@tenpearls.com>
2018-08-10 12:15:00 +0500

    scroll animation on home page added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 declare function require(path: string);
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { Observable } from 'rxjs';
 import * as $ from 'jquery';
@@ -13,10 +13,21 @@ import { delay } from 'q';
 export class HomeComponent implements OnInit {
   imageSrc = require('./img/instagram/ins-1.jpg');
   introImage: Observable<any>;
+  showAnimation = false;
+
   constructor(private fireStorage: AngularFireStorage) {
     const ref = this.fireStorage.ref('intro.jpg');
     this.introImage = ref.getDownloadURL();
   }
+  @ViewChild('test') testElement;
+  @HostListener('window:scroll', [])
+    onscroll() {
+      if (window.pageYOffset >= this.testElement.nativeElement.offsetTop) {
+        this.showAnimation = true;
+      } else {
+        this.showAnimation = false;
+      }
+    }
 
   ngOnInit() {
     $('#preloader').delay(600).fadeOut();
