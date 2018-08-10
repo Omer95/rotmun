311feb710e7624636453c099f084eda8174dd7bc
Omer Farooq <omer.farooq@tenpearls.com>
2018-08-10 12:35:33 +0500

    animation added with scroll event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,17 @@
 declare function require(path: string);
-import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChildren, AfterViewInit } from '@angular/core';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { Observable } from 'rxjs';
 import * as $ from 'jquery';
 import { delay } from 'q';
+import { QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   imageSrc = require('./img/instagram/ins-1.jpg');
   introImage: Observable<any>;
   showAnimation = false;
@@ -19,18 +20,23 @@ export class HomeComponent implements OnInit {
     const ref = this.fireStorage.ref('intro.jpg');
     this.introImage = ref.getDownloadURL();
   }
-  @ViewChild('test') testElement;
+  @ViewChildren('test, test2') testElement: QueryList<any>;
   @HostListener('window:scroll', [])
     onscroll() {
-      if (window.pageYOffset >= this.testElement.nativeElement.offsetTop) {
-        this.showAnimation = true;
-      } else {
-        this.showAnimation = false;
-      }
+      this.testElement.forEach( (anElement) => {
+        if (window.pageYOffset >= anElement.nativeElement.offsetTop) {
+          this.showAnimation = true;
+        } else {
+          this.showAnimation = false;
+        }
+      });
     }
 
   ngOnInit() {
     $('#preloader').delay(600).fadeOut();
+  }
+  ngAfterViewInit() {
+    this.testElement.forEach(el => console.log(el));
   }
 
 }
