68f01136ad874bc40b169fe050fc970da9fac6ed
Omer Farooq Ahmed <omerfarooqali@gmail.com>
2018-08-11 23:40:11 +0500

    first build deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBqP5X9XXyy1-xqwUfxCRCVnCOeJhS6_AI',
+    authDomain: 'rotmun-7646c.firebaseapp.com',
+    databaseURL: 'https://rotmun-7646c.firebaseio.com',
+    projectId: 'rotmun-7646c',
+    storageBucket: 'rotmun-7646c.appspot.com',
+    messagingSenderId: '1095349605349'
+  }
 };
